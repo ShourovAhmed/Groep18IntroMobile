7a5f0ea0e00ec22839750b57a1dfbda7aeb347dc
Thomas <thomasvh1.me@gmail.com>
2022-04-29 14:29:52 +0200

    Home page Admin

--- lib/AdminPage.dart
+++ lib/AdminPage.dart
@@ -7,48 +7,66 @@ class AdminHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.orange,
-          title: const Text('Home Admin'),
-        ),
-        body: Center(
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.orange,
-                    padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                    textStyle: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                  ),
-                  child: Text('Home'),
-                  onPressed: () {
-                    // Navigate to second route when tapped.
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SecondRoute()),
-                    );
-                  },
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text('Home Admin'),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.orange,
+                  padding: const EdgeInsets.symmetric(horizontal: 72, vertical: 20),
+                  textStyle: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                 ),
-                SizedBox(
-                  width: 100,
+                child: Text('Examenvragen'),
+                onPressed: () {
+                  // Navigate to second route when tapped.
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SecondRoute()),
+                  );
+                },
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.lightBlueAccent,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  textStyle: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.orange,
-                    padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                    textStyle: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                  ),
-                  child: Text('Still Home'),
-                  onPressed: () {
-                    // Navigate to second route when tapped.
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SecondRoute()),
-                    );
-                  },
-                ),
-              ]),
-        ));
+                child: Text('Verbeter examens'),
+                onPressed: () {
+                  // Navigate to second route when tapped.
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SecondRoute()),
+                  );
+                },
+              ),
+            ]),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.orange,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box_rounded),
+            label: 'Examen',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment),
+            label: 'Studenten',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.lock),
+            label: 'Wachtwoord wijzigen',
+          ),
+        ],
+      ),
+    );
   }
 }

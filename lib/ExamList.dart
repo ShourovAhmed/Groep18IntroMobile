02653260512e1357a82e6e19475ebbc9057b5c4a
Shourov <shourov.ahmed@student.ap.be>
2022-05-19 09:37:15 +0200

    Fixed buttons layout

--- lib/ExamList.dart
+++ lib/ExamList.dart
@@ -1,4 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:intro_mobile_project/AdminQuestions/OpenVraag.dart';
+import 'Firebase.dart';
+import 'Questions/CodeQuestion.dart';
+import 'Questions/OpenQuestion.dart';
+import 'Questions/OptionQuestion.dart';
+
+final fb = new firebase();
+final index = "";
+final quest = "";
+int counter = 0;
+final List<String> questions = <String>[];
+/*
+@override
+void dispose() {
+  questions.dispose();
+}
+
+ */
+
 
 class ExamList extends StatefulWidget {
   @override
@@ -6,7 +25,7 @@ class ExamList extends StatefulWidget {
 }
 
 class _State extends State<ExamList> {
-  final List<String> questions = <String>[];
+
 
 
   TextEditingController nameController = TextEditingController();
@@ -14,12 +33,14 @@ class _State extends State<ExamList> {
   void addOpen(){
     setState(() {
       questions.insert(questions.length,'Open vraag');
+      fb.AddQuestions('${questions.length}', 'Open vraag');
     });
   }
 
   void addMulti(){
     setState(() {
       questions.insert(questions.length,'Multiple choice');
+      fb.AddQuestions('${questions.length}', 'Multiple choice');
     });
   }
 
@@ -27,6 +48,7 @@ class _State extends State<ExamList> {
   void addCode(){
     setState(() {
       questions.insert(questions.length,'Code vraag');
+      fb.AddQuestions('${questions.length}', 'Code vraag');
     });
   }
 
@@ -49,6 +71,7 @@ class _State extends State<ExamList> {
                           margin: EdgeInsets.all(2),
                           child: Center(
                               child: Text('${questions[index]} (${[index]})',
+
                                 //style: TextStyle(fontSize: 18),
                               )
                           ),
@@ -60,24 +83,65 @@ class _State extends State<ExamList> {
               Expanded(
                 child: Column(
                   children: [
+
+                    SizedBox(
+                        height: 50
+                    ),
                     ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        //primary: Colors.orange,
+                        padding: const EdgeInsets.symmetric(horizontal: 72, vertical: 20),
+                        textStyle: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
                     child: Text('Open vraag'),
+
                     onPressed: () {
-                      addOpen();
+                      //addOpen();
+
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => OpenVraag()),
+                        );
+
+
                     },
                   ),
 
+                  SizedBox(
+                    height: 50
+                  ),
+
                   ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      //primary: Colors.orange,
+                      padding: const EdgeInsets.symmetric(horizontal: 72, vertical: 20),
+                      textStyle: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
                     child: Text('Multiple choice'),
                     onPressed: () {
                       addMulti();
                     },
                   ),
 
+                    SizedBox(
+                        height: 50
+                    ),
+
                     ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        //primary: Colors.orange,
+                        padding: const EdgeInsets.symmetric(horizontal: 72, vertical: 20),
+                        textStyle: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
                       child: Text('Code vraag'),
                       onPressed: () {
                         addCode();
+                        /*
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => QuestionCode()),
+                        );*/
+
                       },
                     ),
 

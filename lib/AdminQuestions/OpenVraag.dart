79a8edcaa7ce56490ff30c93010280109f400cae
Shourov <shourov.ahmed@student.ap.be>
2022-05-19 09:43:32 +0200

    Admin - Added Open Vraag

---
+++ lib/AdminQuestions/OpenVraag.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../ExamList.dart';
+
+class OpenVraag extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: const Text('Admin'),
+        ),
+
+
+
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Open vraag:', style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                    width: 800.0,
+                    child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          //labelText: 'Open vraag',
+                          hintText: 'Geef uw vraag in',
+                        ),
+                        style: TextStyle(
+                            fontSize: 40.0,
+                            height: 2.0,
+                            color: Colors.black
+                        )
+                    )
+                ),
+                SizedBox(height: 50,),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: Size(200,80),
+                      primary: Colors.orange
+                  ),
+                  child: Text("Voeg toe", style: TextStyle(fontSize: (40)),),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ExamList()),
+                    );
+                  },
+                )
+              ],
+            )
+        )
+    );
+  }
+
+}
